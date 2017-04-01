cd7505cefa51747712596a12cb2867027cc32ef0
Luoyuan <2284264985@qq.com>
2017-04-01 17:02:28 +0800

    login

---
+++ index/js/script.js
@@ -0,0 +1,12 @@
+/*
+* @Author: Administrator
+* @Date:   2017-04-01 11:50:54
+* @Last Modified by:   Administrator
+* @Last Modified time: 2017-04-01 14:46:06
+*/
+
+'use strict';
+$(function(){
+	var formHeight = $(window).height();
+	$('.form').css('height', formHeight);
+});
